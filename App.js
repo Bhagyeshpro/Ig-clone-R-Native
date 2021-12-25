615366d0406f126fd8d81dc2774082b4ae8868d4
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-12-25 12:56:53 -0800

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Insta Clone One Day!</Text>
       <StatusBar style="auto" />
     </View>
   );
